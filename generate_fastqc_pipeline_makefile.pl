6579981acf86bb81f78cd86f4455b708fd0c5a9c
Adrian Tan <atks@umich.edu>
2020-10-12 23:42:55 +0800

    added named jobs

--- generate_fastqc_pipeline_makefile.pl
+++ generate_fastqc_pipeline_makefile.pl
@@ -38,7 +38,7 @@ my $outputDir;
 my $splitLineNo;
 my $makeFile;
 my $refGenomeDir;
-my $qsubJobName;
+my $pipelineName;
 
 #initialize options
 Getopt::Long::Configure ('bundling');
@@ -47,7 +47,7 @@ if(!GetOptions ('h'=>\$help,
                 'o=s'=>\$outputDir,
                 's=s'=>\$sampleFile,
                 'd=i'=>\$splitLineNo,
-                'n:s'=>\$qsubJobName,
+                'n:s'=>\$pipelineName,
                 'm:s'=>\$makeFile,
                 'r:s'=>\$refGenomeDir
                )
@@ -55,6 +55,7 @@ if(!GetOptions ('h'=>\$help,
   || !defined($splitLineNo)
   || !defined($sampleFile)
   || !defined($makeFile)
+  || !defined($pipelineName)
   || !defined($refGenomeDir))
 {
     if ($help)
@@ -79,7 +80,7 @@ my $trimGalore = "/home/users/ntu/adrianta/programs/trimGalore-0.4.5/trim_galore
 my $cutAdaptPath = "/home/users/ntu/adrianta/programs/cutadapt-1.15";
 my $cutAdapt = "/home/users/ntu/adrianta/programs/cutadapt-1.15/cutadapt";
 my $zsplit = "/home/users/ntu/adrianta/programs/NTU-pipeline/zsplit.pl";
-my $bismarck = "/home/users/ntu/adrianta/programs/programs/bismark-0.19.0";
+my $bismark = "/home/users/ntu/adrianta/programs/bismark-0.19.0/bismark";
 
 printf("generate_bismarck_pipeline_makefile.pl\n");
 printf("\n");
@@ -87,7 +88,7 @@ printf("options: output dir           %s\n", $outputDir);
 printf("         make file            %s\n", $makeFile);
 printf("         split line no        %s\n", $splitLineNo);
 printf("         sample file          %s\n", $sampleFile);
-printf("         reference            %s\n", $refWholeSulfiteGenomeFASTAFile);
+printf("         reference            %s\n", $refGenomeDir);
 printf("\n");
 
 #this pipeline generator generates 2 makefiles
@@ -151,80 +152,8 @@ for my $sampleID (@SAMPLE)
     mkpath("$outputDir/samples/$sampleID/fastqc_output");
     mkpath("$outputDir/samples/$sampleID/trim_galore_output");    
     mkpath("$outputDir/samples/$sampleID/trimmed_fastqc_output");
+    mkpath("$outputDir/samples/$sampleID/aligned");   
 }
-
-################################################
-#Create/update/read augmented sample file
-#
-#a. lines_counted.OK exists and
-#   a. augmented.sa exists   
-#      a. age(lines_counted.OK) >  age(augmented.sa)    => overwrite
-#      b. age(lines_counted.OK) <= age(augmented.sa)    => read                       
-#   b. augmented.sa !exists                             => create     
-#b. lines_counted.OK !exists                            => skip
-#
-################################################
-
-###read augmented sample file
-##if (-e $linesCountedFile)
-##{
-##    my $linesCountedFile = "$outputDir/lines_counted.OK";
-##    my $lastTimeLinesCounted = `stat -t --printf="%Y" $linesCountedFile`;
-##    my $createOrWriteAugmentedSampleFile = 0;
-##    
-##    if (-e $augmentedSampleFile)
-##    {
-##        my $lastTimeAugmentedSampleFileModified = `stat -t --printf="%Y" $augmentedSampleFile`;
-##        
-##        if ($lastTimeLinesCounted>$lastTimeAugmentedSampleFileModified)
-##        {
-##            $createOrWriteAugmentedSampleFile = 1;
-##        }
-##        else
-##        {
-##            #read augmented sample file
-##            open(SA,"$sampleFile") || die "Cannot open $sampleFile\n";
-##            while (<SA>)
-##            {
-##                s/\r?\n?$//;
-##                if(!/^#/)
-##                {
-##                    my ($sampleID, $fastq1Path, $fastq2Path, $noLines) = split(/\s+/, $_);
-##                    
-##                    if (exists($SAMPLE{$sampleID}))
-##                    {
-##                        $SAMPLE{$sampleID}{NO_LINES} = $noLines;
-##                    }
-##                    else
-##                    {
-##                        warn("$sampleID not in the sample file submitted. This sample is ignored.");
-##                    }
-##                }
-##            }
-##            close(SA);
-##        }
-##    }
-##    else
-##    {
-##        $createOrWriteAugmentedSampleFile = 1;
-##    }
-##    
-##    #create/overwrite augmented sample file
-##    if ($createOrWriteAugmentedSampleFile)
-##    {
-##        open(SA, ">$augmentedSampleFile") || die "Cannot open $augmentedSampleFile\n";
-##        for my $sampleID (@SAMPLE)
-##        {
-##            my $lines = `cat "$outputDir/samples/$sampleID/no_lines.txt"`;
-##            print SA "$sampleID\t$SAMPLE{$sampleID}{FASTQ1}\t$SAMPLE{$sampleID}{FASTQ2}\t$lines\n";
-##        }
-##        close(SA);  
-##    }
-##}
-##else
-##{
-##    
-##}
 
 #####################
 #look for file counts
@@ -271,20 +200,12 @@ if ($noFilesToBeCounted)
         $dep = "$SAMPLE{$sampleID}{FASTQ1} $SAMPLE{$sampleID}{FASTQ2}";
         $log = "$outputDir/split.log";
         $err = "$outputDir/split.err";
-        @cmd = ("$zsplit -s $sampleID -o $outputDir -l 40000000 -c $outputFile $SAMPLE{$sampleID}{FASTQ1} $SAMPLE{$sampleID}{FASTQ2}");
-        #@cmd = ("$zsplit -s $sampleID -o $outputDir -l 2000000 -c $outputFile $SAMPLE{$sampleID}{FASTQ2} $SAMPLE{$sampleID}{FASTQ2}");
-        makePBSCommandLine($tgt, $dep, $log, $err, "24:00:00", @cmd);
+        @cmd = ("$zsplit -s $sampleID -o $outputDir -l $splitLineNo -c $outputFile $SAMPLE{$sampleID}{FASTQ1} $SAMPLE{$sampleID}{FASTQ2}");
+        makeJob("namedPBS", $pipelineName, $tgt, $dep, $log, $err, "24:00:00", @cmd);
     }
-       
-#    $tgt = "$outputDir/split.OK";
-#    $dep = "$noLinesOKFiles";
-#    @cmd = ("echo $noFilesToBeCounted files counted.");
-#    makeLocalStep($tgt, $dep, @cmd);
     
     $makeFile = "$outputDir/count_lines.mk";
     print "Please run \"make -f count_lines.mk -j 8 -k\n";
-
-
     goto GENERATE_MAKEFILE;
 }
 
@@ -301,23 +222,26 @@ for my $sampleID (@SAMPLE)
     $log = "$fastqcOutputDir/fastqc1.log";
     $err = "$fastqcOutputDir/fastqc1.err";
     @cmd = ("$fastqc $SAMPLE{$sampleID}{FASTQ1} -o $fastqcOutputDir");
-    makePBSCommandLine($tgt, $dep, $log, $err, "24:00:00", @cmd);
+    makeJob("namedPBS", $pipelineName, $tgt, $dep, $log, $err, "24:00:00", @cmd);
     $tgt = "$fastqcOutputDir/fastqc2.OK";
     $dep = "$SAMPLE{$sampleID}{FASTQ2}";
     $log = "$fastqcOutputDir/fastqc2.log";
     $err = "$fastqcOutputDir/fastqc2.err";
     @cmd = ("$fastqc $SAMPLE{$sampleID}{FASTQ2} -o $fastqcOutputDir");
-    makePBSCommandLine($tgt, $dep, $log, $err, "24:00:00", @cmd);
+    makeJob("namedPBS", $pipelineName, $tgt, $dep, $log, $err, "24:00:00", @cmd);
     
     ###########
     #trimgalore
     ###########
-    my $noFile = ceil($SAMPLE{$sampleID}{NO_LINES}/40000000);
+    my $noFile = ceil($SAMPLE{$sampleID}{NO_LINES}/$splitLineNo);
     
     print "$sampleID \t no files = $noFile\n";
     my $splitTrimmedOKFiles = "";
     my $splitTrimmedR1FASTQFiles = "";
     my $splitTrimmedR2FASTQFiles = "";
+   
+    my $splitTrimmedAlignedOKFiles = "";
+    my $splitTrimmedAlignedBAMFiles = "";
     for my $i (1 .. $noFile)
     {
         my $splitDir = "$outputDir/samples/$sampleID/split";
@@ -333,12 +257,6 @@ for my $sampleID (@SAMPLE)
         my $trimmedR1File = "$trimGaloreOutputDir/$i" . "_$file1" . "_val_1.fq.gz";
         my $trimmedR2File = "$trimGaloreOutputDir/$i" . "_$file2" . "_val_2.fq.gz";
         
-#        print "CHECK: $file1, $dir1, $suffix1\n";
-#        print "     : $R1File\n";
-#        print "     : $R2File\n";
-#        print "     : $trimmedR1File\n";
-#        print "     : $trimmedR2File\n";
-        
         ##############
         #trimmed reads
         ##############
@@ -351,43 +269,43 @@ for my $sampleID (@SAMPLE)
                             "--path_to_cutadapt $cutAdapt " .
                             "--keep --illumina --clip_R2 18 --three_prime_clip_R1 18 --phred33 " .
                             "--paired $R1File $R2File");
-        makePBSCommandLine($tgt, $dep, $log, $err, "24:00:00", @cmd);
-        
+        makeJob("namedPBS", $pipelineName, $tgt, $dep, $log, $err, "24:00:00", @cmd);
         
         ####################
         #align trimmed reads
         ####################
-        $tgt = "$alignedOutputDir/.OK";
+        my $alignedOutputDir = "$outputDir/samples/$sampleID/aligned/$i";
+        my $tempAlignedOutputDir = "$alignedOutputDir/temp";
+        $tgt = "$alignedOutputDir/aligned.OK";
+        $splitTrimmedAlignedOKFiles .= $i==0 ? "$alignedOutputDir/aligned.OK" : " $alignedOutputDir/aligned.OK";
+        $splitTrimmedAlignedBAMFiles .= $i==0 ? "$alignedOutputDir/aligned.OK" : " $alignedOutputDir/aligned.OK";
+   
         $dep = "$trimGaloreOutputDir/trim_galore.OK";
         $log = "$alignedOutputDir/aligned.log";
         $err = "$alignedOutputDir/aligned.err";
-        @cmd = ("$bismark --bowtie -p 4 --bam " . 
-                    "--temp_dir $tempDir --un --ambiguous " . 
+        @cmd = ("$bismark --bowtie2 -p 4 --bam " . 
+                    "--temp_dir $tempAlignedOutputDir --un --ambiguous " . 
                     "--score_min L,0,-0.2 " .
                     "--path_to_bowtie $bowtie2Path " .
                     "-o $alignedOutputDir " . 
-                    "--genome_folder $genomeRefPath " .
-                    "$trimmedR2File -o $fastqcOutputDir");
-        makePBSCommandLine($tgt, $dep, $log, $err, "24:00:00", @cmd);    
+                    "--genome_folder $refGenomeDir " .
+                    "-1 $trimmedR1File -2 $trimmedR2File");
+        makeJob("namedPBS", $pipelineName, $tgt, $dep, $log, $err, "24:00:00", @cmd);    
         
-    
-#    ### Run bismark alignment on paired-end trimmed fastqs (read_1 & read_2) using bowtie-2:
-#    $BISMARK_PATH/bismark --bowtie2 
-#    -p 4 --bam 
-#    --temp_dir $TEMP_DIR 
-#    --un 
-#    --ambiguous 
-#    --score_min L,0,-0.2 
-#    --path_to_bowtie $BOWTIE_PATH/ 
-#    -o $BAM_OUTPUT_DIR 
-#    --genome_folder $GENOME_PATH 
-#    -1 $TRIMMED_R1 -2 $TRIMMED_R2
         
     }
 
-    ##########################
-    #combine all gzipped files
-    ##########################
+    ######################
+    #combined aligned bams
+    ######################
+
+
+#$SAMTOOLS_PATH/samtools merge -nf $UNSORTED_BAM_DIR/${LIB}_unsorted_merged.bam $BAM_LIST
+
+
+    ############################
+    #combine trimmed fastq files
+    ############################
     my $trimGaloreOutputDir = "$outputDir/samples/$sampleID/trim_galore_output";
     
     my $trimmedR1File = "$trimGaloreOutputDir/trimmed_$sampleID" . "_1.fastq.gz";
@@ -396,7 +314,7 @@ for my $sampleID (@SAMPLE)
     $log = "$trimGaloreOutputDir/zcat.log";
     $err = "$trimGaloreOutputDir/zcat.err";
     @cmd = ("zcat $splitTrimmedR1FASTQFiles | gzip -c > $trimmedR1File");
-    makePBSCommandLine($tgt, $dep, $log, $err, "24:00:00", @cmd);
+    makeJob("namedPBS", $pipelineName, $tgt, $dep, $log, $err, "24:00:00", @cmd);
     
     my $trimmedR2File = "$trimGaloreOutputDir/trimmed_$sampleID" . "_2.fastq.gz";
     $tgt = "$trimmedR2File.OK";
@@ -404,109 +322,25 @@ for my $sampleID (@SAMPLE)
     $log = "$trimGaloreOutputDir/zcat.log";
     $err = "$trimGaloreOutputDir/zcat.err";
     @cmd = ("zcat $splitTrimmedR2FASTQFiles | gzip -c > $trimmedR2File");
-    makePBSCommandLine($tgt, $dep, $log, $err, "24:00:00", @cmd);
+    makeJob("namedPBS", $pipelineName, $tgt, $dep, $log, $err, "24:00:00", @cmd);
         
     #####################
     #fastQC trimmed files
     #####################
-    my $fastqcOutputDir = "$outputDir/samples/$sampleID/trimmed_fastqc_output";
+    $fastqcOutputDir = "$outputDir/samples/$sampleID/trimmed_fastqc_output";
     $tgt = "$fastqcOutputDir/fastqc1.OK";
     $dep = "$trimmedR1File.OK";
     $log = "$fastqcOutputDir/fastqc1.log";
     $err = "$fastqcOutputDir/fastqc1.err";
     @cmd = ("$fastqc $trimmedR1File -o $fastqcOutputDir");
-    makePBSCommandLine($tgt, $dep, $log, $err, "24:00:00", @cmd);
+    makeJob("namedPBS", $pipelineName, $tgt, $dep, $log, $err, "24:00:00", @cmd);
     $tgt = "$fastqcOutputDir/fastqc2.OK";
     $dep = "$trimmedR2File.OK";
     $log = "$fastqcOutputDir/fastqc2.log";
     $err = "$fastqcOutputDir/fastqc2.err";
     @cmd = ("$fastqc $trimmedR2File -o $fastqcOutputDir");
-    makePBSCommandLine($tgt, $dep, $log, $err, "24:00:00", @cmd);    
+    makeJob("namedPBS", $pipelineName, $tgt, $dep, $log, $err, "24:00:00", @cmd);    
 }
-
-goto GENERATE_MAKEFILE;
-
-###################
-##trimgalore
-###################
-#for my $sampleID (@SAMPLE)
-#{
-#    if (!exists($SAMPLE{$sampleID}{NO_LINES}))
-#    {
-#        my $trimGaloreOutputDir = "$outputDir/samples/$sampleID/trim_galore_output";
-#        $tgt = "$trimGaloreOutputDir/trim_galore.OK";
-#        $dep = "$SAMPLE{$sampleID}{FASTQ1} $SAMPLE{$sampleID}{FASTQ2}";
-#        $log = "$trimGaloreOutputDir/trimgalore.log";
-#        $err = "$trimGaloreOutputDir/trimgalore.err";
-#        @cmd = ("$trimGalore -o $trimGaloreOutputDir " .
-#                            "--path_to_cutadapt $cutAdapt " .
-#                            "--illumina --clip_R2 18 --three_prime_clip_R1 18 --phred33 " .
-#                            "--paired $SAMPLE{$sampleID}{FASTQ1} $SAMPLE{$sampleID}{FASTQ2}");
-#        makePBSCommandLine($tgt, $dep, $log, $err, "48:00:00", @cmd);
-#    }
-#}
-
-
-#$BISMARK_PATH/bismark_genome_preparation --path_to_bowtie $BOWTIE_PATH --verbose $GENOME_PATH
-#/home/users/ntu/adrianta/programs/bismark-0.19.0/bismark_genome_preparation --path_to_bowtie  /app/bowtie2/2.29 --verbose ~/ref/
-
-#my $bamListFile = "$auxDir/bam.list";
-#open(OUT,">$bamListFile") || die "Cannot open $bamListFile\n";
-#print OUT $bamFiles;
-#close(OUT);
-#
-#print "read in " . scalar(keys(%SAMPLE)) . " samples\n";
-
-#qsub -v LIB=$LIB $PBS_O_WORKDIR/processfastqc-raw.sh
-##########
-#Alignment
-##########
-
-#print "adding aligning steps\n";
-
-#**************
-#log start time
-#**************
-#$tgt = "$logDir/start.calling.OK";
-#$dep = "";
-#@cmd = ("date | awk '{print \"samtools variant calling pipeline\\n\\nstart calling: \"\$\$0}' > $logFile");
-#makeLocalStep($tgt, $dep, @cmd);
-#
-
-
-
-#************
-#log end time
-#************
-#$tgt = "$logDir/end.calling.OK";
-##$dep = "$intervalVCFFilesOK";
-#@cmd = ("date | awk '{print \"end: \"\$\$0}' >> $logFile");
-#makeLocalStep($tgt, $dep, @cmd);
-
-###########################################
-#Concatenate, normalize and drop duplicates
-###########################################
-
-#**************
-#log start time
-#**************
-#$tgt = "$logDir/start.concatenating.normalizing.OK";
-#$dep = "$logDir/end.calling.OK";
-#@cmd = ("date | awk '{print \"start concatenating and normalizing: \"\$\$0}' >> $logFile");
-#makeLocalStep($tgt, $dep, @cmd);
-
-
-#my $inputVCFFiles = join(" ", map {"$finalVCFOutDir/$_.genotypes.vcf.gz"} @CHROM);
-#my $inputVCFFilesOK = join(" ", map {"$finalVCFOutDir/$_.genotypes.vcf.gz.OK"} @CHROM);
-
-
-#************
-#log end time
-#************
-#$tgt = "$logDir/end.concatenating.normalizing.OK";
-#$dep = "$inputVCFFile.tbi.OK";
-#@cmd = ("date | awk '{print \"end concatenating and normalizing: \"\$\$0}' >> $logFile");
-#makeLocalStep($tgt, $dep, @cmd);
 
 #*******************
 #Write out make file
@@ -536,41 +370,69 @@ close MAK;
 #run a job either locally or by pbs
 sub makeJob
 {
-    my ($method, $tgt, $dep, @cmd) = @_;
+    my ($method, @others) = @_;
+#    print "method: $method \n";
+#    print "\@other: @others \n";
 
     if ($method eq "local")
     {
-        makeLocalStep($tgt, $dep, @cmd);
+        my ($tgt, $dep, @rest) = @others;
+        makeLocalStep($tgt, $dep, @rest);
+    }
+    elsif ($method eq "pbs")
+    {
+        my ($tgt, $dep, @rest) = @others;
+        makePBSStep($tgt, $dep, @rest);
+    }
+    elsif ($method eq "namedPBS")
+    {
+        
+        my ($name, $tgt, $dep, @rest) = @others;  
+        
+#        print "\t name: $name \n";
+#        print "\t tgt: $tgt \n";
+#        print "\t dep: $dep \n";
+#        print "\t \@rest: @rest \n";
+              
+        makeNamedPBSStep($name, $tgt, $dep, @rest);
     }
     else
     {
-        makePBS($tgt, $dep, @cmd);
+        die "unrecognized method of job creation : $method\n";
     }
 }
 
-#run PBS jobs
-sub makePBSCommandLine
+sub makePBSStep
 {
     my ($tgt, $dep, $log, $err, $walltime, @cmd) = @_;
     push(@tgts, $tgt);
     push(@deps, $dep);
-    my $cmd = "";
+    my $cmd = join(";", @cmd);
     $cmd = "set -o pipefail;" . join(";", @cmd);
     $cmd = "\techo \"$cmd\" | qsub -q normal -P 12000713 -W block=true -o $log -e $err -l select=1:ncpus=1,walltime=$walltime\n";
     $cmd .= "\ttouch $tgt\n";
     push(@cmds, $cmd);
 }
 
-#run PBS jobs
-sub makeNamedPBSCommandLine
+sub makeNamedPBSStep
 {
-    my ($tgt, $dep, $log, $err, $walltime, $qsubJobName, @cmd) = @_;
+    my ($name, $tgt, $dep, $log, $err, $walltime, @cmd) = @_;
+
+#    print "\t\t name: $name \n";
+#    print "\t\t tgt: $tgt \n";
+#    print "\t\t dep: $dep \n";
+#    print "\t\t log: $log \n";
+#    print "\t\t err: $err \n";
+#    print "\t\t wt : $walltime \n";
+#    print "\t\t \@cmd: @cmd \n";
+        
     push(@tgts, $tgt);
     push(@deps, $dep);
-    my $cmd = "";
+    my $cmd = join(";", @cmd);
     $cmd = "set -o pipefail;" . join(";", @cmd);
-    $cmd = "\techo \"$cmd\" | qsub -q normal -P 12000713 -W block=true -N qsubJobName -o $log -e $err -l select=1:ncpus=1,walltime=$walltime\n";
+    $cmd = "\techo \"$cmd\" | qsub -q normal -P 12000713 -W block=true -N $name -o $log -e $err -l select=1:ncpus=1,walltime=$walltime\n";
     $cmd .= "\ttouch $tgt\n";
+    
     push(@cmds, $cmd);
 }
 
